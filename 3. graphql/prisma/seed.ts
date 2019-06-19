d95b39e19f96f4a3af0034d68623625790060117
Rohit <rohit@novvuminc.com>
2019-06-19 01:02:49 -0700

    updated seeds

--- 3. graphql/prisma/seed.ts
+++ 3. graphql/prisma/seed.ts
@@ -5,11 +5,13 @@ const photon = new Photon();
 async function main() {
   await photon.heroes.create({
     data: {
-      email: "alice@prisma.io",
-      name: "Alice",
+      email: "captain.marvel@marvel.com",
+      name: "Captain Marvel",
       movies: {
         create: {
-          title: "Join us for Prisma Day 2019 in Berlin",
+          title: "Captain Marvel",
+          description:
+            "Carol Danvers becomes one of the universe's most powerful heroes when Earth is caught in the middle of a galactic war between two alien races.",
           released: true
         }
       }
@@ -17,16 +19,16 @@ async function main() {
   });
   await photon.heroes.create({
     data: {
-      email: "bob@prisma.io",
+      email: "bob@marvel.com",
       name: "Bob",
       movies: {
         create: [
           {
-            title: "Subscribe to GraphQL Weekly for community news",
+            title: "Bob's World",
             released: true
           },
           {
-            title: "Follow Prisma on Twitter",
+            title: "Bob's World 2",
             released: false
           }
         ]
