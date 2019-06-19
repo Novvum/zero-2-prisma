b27beabf438ae760035da17aedc8a354618438dc
rajinwonderland <raj@novvuminc.com>
2019-06-19 09:21:06 +0200

    Updates

--- 2. grpc/prisma/seed.ts
+++ 2. grpc/prisma/seed.ts
@@ -1,50 +1,38 @@
-import Photon, { MovieCreateInput } from '@generated/photon';
-import { mcu } from '../data/mcu';
-import { characters } from '../data/characters';
+import Photon from '@generated/photon';
 
-const movies = mcu.map((movie) => {
-	const char = [];
-	movie.ironMan &&
-		char.push({
-			create: characters[0]
-		});
-	movie.thor && char.push(characters[1]);
-	movie.hulk && char.push(characters[2]);
-	movie.blackPanther && char.push(characters[3]);
-	movie.captainMarvel && char.push(characters[4]);
-	movie.blackWidow && char.push(characters[5]);
-	movie.nickFury && char.push(characters[6]);
-	movie.spiderman && char.push(characters[7]);
-	movie.guardiansOfGalaxy && char.push(characters[8]);
-	movie.thanos && char.push(characters[9]);
-	movie.drStrange && char.push(characters[10]);
-	movie.captainAmerica && char.push(characters[11]);
-	let res: any = {};
-	for (var i = 0; i < char.length; i++) {
-		res.create = +char[i];
-	}
-	const m = {
-		film: movie.film,
-		releaseYear: movie.releaseYear,
-		budget: movie.budget,
-		runningTime: movie.runningTime,
-		domesticGross: movie.domesticGross,
-		weekendGross: movie.weekendGross,
-		overseasGross: movie.overseasGross,
-		worldwideGross: movie.worldwideGross
-	};
-	return char.length > 1 ? { ...m, characters: res } : m;
-});
 const photon = new Photon();
 
 async function main() {
-	const films = await movies.map(
-		async (data) =>
-			await photon.movies.create({
-				data
-			})
-	);
-	console.log({ films });
+	await photon.heroes.create({
+		data: {
+			email: 'alice@prisma.io',
+			name: 'Alice',
+			movies: {
+				create: {
+					title: 'Join us for Prisma Day 2019 in Berlin',
+					released: true
+				}
+			}
+		}
+	});
+	await photon.heroes.create({
+		data: {
+			email: 'bob@prisma.io',
+			name: 'Bob',
+			movies: {
+				create: [
+					{
+						title: 'Subscribe to GraphQL Weekly for community news',
+						released: true
+					},
+					{
+						title: 'Follow Prisma on Twitter',
+						released: false
+					}
+				]
+			}
+		}
+	});
 }
 
 main()
