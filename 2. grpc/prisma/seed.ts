9b5a582acc30bd1642470ca94d0341933d765bbc
rajinwonderland <raj@novvuminc.com>
2019-06-19 07:00:50 +0200

    Updates

--- 2. grpc/prisma/seed.ts
+++ 2. grpc/prisma/seed.ts
@@ -1,48 +1,54 @@
-import Photon from '@generated/photon'
+import Photon, { MovieCreateInput } from '@generated/photon';
+import { mcu } from '../data/mcu';
+import { characters } from '../data/characters';
 
-const photon = new Photon()
+const movies = mcu.map((movie) => {
+	const char = [];
+	movie.ironMan &&
+		char.push({
+			create: characters[0]
+		});
+	movie.thor && char.push(characters[1]);
+	movie.hulk && char.push(characters[2]);
+	movie.blackPanther && char.push(characters[3]);
+	movie.captainMarvel && char.push(characters[4]);
+	movie.blackWidow && char.push(characters[5]);
+	movie.nickFury && char.push(characters[6]);
+	movie.spiderman && char.push(characters[7]);
+	movie.guardiansOfGalaxy && char.push(characters[8]);
+	movie.thanos && char.push(characters[9]);
+	movie.drStrange && char.push(characters[10]);
+	movie.captainAmerica && char.push(characters[11]);
+	let res: any = {};
+	for (var i = 0; i < char.length; i++) {
+		res.create = +char[i];
+	}
+	const m = {
+		film: movie.film,
+		releaseYear: movie.releaseYear,
+		budget: movie.budget,
+		runningTime: movie.runningTime,
+		domesticGross: movie.domesticGross,
+		weekendGross: movie.weekendGross,
+		overseasGross: movie.overseasGross,
+		worldwideGross: movie.worldwideGross
+	};
+	return char.length > 1 ? { ...m, characters: res } : m;
+});
+const photon = new Photon();
 
 async function main() {
-  const user1 = await photon.users.create({
-    data: {
-      email: 'alice@prisma.io',
-      name: 'Alice',
-      password: 'alice',
-      posts: {
-        create: {
-          title: 'Join us for Prisma Day 2019 in Berlin',
-          content: 'https://www.prisma.io/day/',
-          published: true,
-        },
-      },
-    },
-  })
-  const user2 = await photon.users.create({
-    data: {
-      email: 'bob@prisma.io',
-      name: 'Bob',
-      password: 'alice',
-      posts: {
-        create: [
-          {
-            title: 'Subscribe to GraphQL Weekly for community news',
-            content: 'https://graphqlweekly.com/',
-            published: true,
-          },
-          {
-            title: 'Follow Prisma on Twitter',
-            content: 'https://twitter.com/prisma',
-            published: false,
-          },
-        ],
-      },
-    },
-  })
-  console.log({ user1, user2 })
+	const films = await movies.map(
+		async (data) =>
+			await photon.movies.create({
+				data
+			})
+	);
+	console.log({ films });
 }
 
 main()
-  .catch(e => console.error(e))
-  .finally(async () => {
-    await photon.disconnect()
-  })
+	.catch((e) => console.error(e))
+	.finally(async () => {
+		await photon.disconnect();
+	});
